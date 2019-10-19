645bf8f35ad4ff8b365273437141a1c8f3131eb1
Ali Alsayed <a_alsayed87@hotmail.com>
2019-10-19 07:48:26 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -45,7 +45,7 @@ export class ButcherShop extends Component {
     return (
       <div className='butcher-shop'>
         <p>Hello! We have the following products for sale today:</p>
-        
+
       </div>
     )
   }
